937087e9072fcd59743f1f04417a0e0e97d6a6fa
Martason <annamarta.hjalmarson@utb.ecutbildning.se>
2023-01-07 10:55:51 +0100

    removed content in strapi

--- Psykologkompassen-Headless/backend/src/api/organisation/controllers/organisation.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * organisation controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::organisation.organisation');
--- Psykologkompassen-Headless/backend/src/api/organisation/routes/organisation.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * organisation router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::organisation.organisation');
